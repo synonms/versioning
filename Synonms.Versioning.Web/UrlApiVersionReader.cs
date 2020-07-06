4f591d2ac0de0ca2b8ddc96d87cb3ce7a5284c54
Nick Sherriff <syno.nms74@outlook.com>
2020-07-06 14:38:52 +0100

    URL version reader fix for versions with single numeric component e.g v1

--- Synonms.Versioning.Web/UrlApiVersionReader.cs
+++ Synonms.Versioning.Web/UrlApiVersionReader.cs
@@ -12,10 +12,23 @@ namespace Synonms.Versioning.Web
 
             var segments = path.Value.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (segments.Length > 0 && Version.TryParse(segments[0].TrimStart(new[] { 'v','V' }), out var segment0ApiVersion)) return segment0ApiVersion;
-            if (segments.Length > 1 && Version.TryParse(segments[1].TrimStart(new[] { 'v', 'V' }), out var segment1ApiVersion)) return segment1ApiVersion;
+            if (segments.Length == 0) return default;
+
+            if (Version.TryParse(FormatSegment(segments[0]), out var segment0ApiVersion)) return segment0ApiVersion;
+
+            if (segments.Length == 1) return default;
+            
+            if (Version.TryParse(FormatSegment(segments[1]), out var segment1ApiVersion)) return segment1ApiVersion;
 
             return default;
+        }
+
+        private string FormatSegment(string segment)
+        {
+            var formattedSegment = segment.TrimStart(new[] { 'v', 'V' });
+            if (!formattedSegment.Contains(".")) formattedSegment += ".0";
+
+            return formattedSegment;
         }
     }
 }
